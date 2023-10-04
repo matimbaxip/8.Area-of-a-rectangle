5c03cc1fa125ab607a40f8fc667645978e3f038a
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-04 11:22:02 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim L As Double
+        Dim H As Double
+        Dim A As Double
+        L = Val(TextBox1.Text)
+        H = Val(TextBox2.Text)
+        A = L * H
+        TextBox3.Text = A
+    End Sub
+End Class
